a134fa1eeb9e3b86a3fa6d777b664bb489421768
corda1235 <cordario@gmail.com>
2025-05-16 12:17:01 +0200

    Create __init__.py

---
+++ src/monitoring/__init__.py
@@ -0,0 +1,12 @@
+# File: src/monitoring/__init__.py
+
+# -*- coding: utf-8 -*-
+
+"""
+Monitoring module for the Comtrade Data Pipeline.
+Provides monitoring and alerting functionality.
+"""
+
+from .monitor import PipelineMonitor
+
+__all__ = ['PipelineMonitor']
